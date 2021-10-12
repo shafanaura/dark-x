a8839b9621cd2cbc181a86b965c2f76bb4a31d96
shafanaura <shafanaura48@gmail.com>
2021-10-12 15:23:51 +0700

    fix grid

--- layouts/Section_2.js
+++ layouts/Section_2.js
@@ -6,48 +6,50 @@ export default function Section_2() {
   return (
     <>
       <div className={styles.container}>
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="arrow"
-          title="Realtime Trading"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="chart"
-          title="Advanced Charts"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="security"
-          title="Enterprise Grade Security"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="chart-up"
-          title="ETFs and stocks"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="bag"
-          title="Currencies & Commodities"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
-        <CardCategory
-          variant="bg"
-          direction="col"
-          icon="bitcoin"
-          title="Cyrptocurrencies"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
-        />
+        <div className={styles.content}>
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="arrow"
+            title="Realtime Trading"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="chart"
+            title="Advanced Charts"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="security"
+            title="Enterprise Grade Security"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="chart-up"
+            title="ETFs and stocks"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="bag"
+            title="Currencies & Commodities"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+          <CardCategory
+            variant="bg"
+            direction="col"
+            icon="bitcoin"
+            title="Cyrptocurrencies"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."
+          />
+        </div>
       </div>
       <div className={styles.btn__container}>
         <Button size="large">Get Started</Button>
