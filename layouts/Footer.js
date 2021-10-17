5a0043c06b7146a14eeb3e3d18dd8e85e820711d
shafanaura <shafanaura48@gmail.com>
2021-10-17 11:38:22 +0700

    init footer

---
+++ layouts/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <hr />
+      dadw
+      <hr />
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styles from '../styles/Home.module.scss'
 import Head from 'next/Head'
 import Navbar from '../components/Navbar'
 import Introduction from '../layouts/Introduction'
@@ -6,10 +5,11 @@ import Feature from '../layouts/Feature'
 import About from '../layouts/About'
 import Started from '../layouts/Started'
 import Review from '../layouts/Review'
+import Footer from '../layouts/Footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Dark X</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -22,6 +22,8 @@ export default function Home() {
         <Started />
         <Review />
       </div>
+      <div style={{ marginTop: 100 }} />
+      <Footer />
       <div style={{ marginTop: 100 }} />
     </div>
   )
