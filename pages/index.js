9f0819d2d67d85ac55f0bc65f7d73734d7320898
shafanaura <shafanaura48@gmail.com>
2021-10-18 09:29:01 +0700

    change to next/head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/Head'
+import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Introduction from '../layouts/Introduction'
 import Feature from '../layouts/Feature'
